c6b605a7e94ac5fc460d4e179bfbaf478f4ad6f4
Qing Ye <qing.ye@skyscanner.net>
2019-07-07 00:28:28 +0800

    feat: add support for content blocks

---
+++ test/integration/braze/tags/contentBlocks.ts
@@ -0,0 +1,166 @@
+import Liquid from '../../../../src/liquid'
+import { expect, should, use } from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+import {mock, restore} from "../../../stub/mockfs";
+
+should()
+use(chaiAsPromised)
+
+describe('braze/tags/content_blocks', function () {
+  const liquid = new Liquid()
+
+  afterEach(restore)
+  it('should support content blocks in current dir', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_block': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in current dir with default .liquid ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_block.liquid': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in current dir with custom ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_block.html': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        ext: '.html'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in content_blocks dir', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_blocks/content_block': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in content_blocks dir with default .liquid ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_blocks/content_block.liquid': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in content_blocks dir with custom ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_blocks/content_block.html': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        ext: '.html'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in ../content_blocks dir', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '../content_blocks/content_block': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in ../content_blocks dir with default .liquid ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '../content_blocks/content_block.liquid': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in ../content_blocks dir with custom ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '../content_blocks/content_block.html': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        ext: '.html'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in custom dir', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '/dir/content_block': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        root: '/dir'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in custom dir with default .liquid ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '/dir/content_block.liquid': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        root: '/dir'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support content blocks in custom dir with custom ext', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      '/dir/content_block.html': 'bar'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      '__contentBlocks': {
+        root: '/dir',
+        ext: '.html'
+      }
+    })
+    return expect(html).to.equal('foobar')
+  })
+
+  it('should support nested content blocks', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_block': 'bar{{content_blocks.${content_block_2}}}',
+      './content_block_2': '2'
+    })
+    const html = await liquid.renderFile('./current.liquid')
+    return expect(html).to.equal('foobar2')
+  })
+
+  it('should pass context to content blocks', async function () {
+    mock({
+      './current.liquid': 'foo{{content_blocks.${content_block}}}',
+      './content_block': 'bar{{var}}'
+    })
+    const html = await liquid.renderFile('./current.liquid', {
+      var: 'value_of_var'
+    })
+    return expect(html).to.equal('foobarvalue_of_var')
+  })
+})
